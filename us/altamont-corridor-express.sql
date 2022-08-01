c000900d6814f7b505d45551699198ca07d47e3e
Peter Storch <peter.storch@tuta.io>
2022-08-01 23:42:01 +0200

    Add stationId and insert statements

---
+++ us/altamont-corridor-express.sql
@@ -0,0 +1,5 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1478', 'Lathrop/Manteca', 37.797908, -121.263664);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1479', 'Livermore', 37.685105, -121.767495);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1480', 'Pleasanton', 37.657549, -121.882962);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1481', 'Tracy', 37.696468, -121.433869);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1482', 'Vasco Road', 37.697081, -121.717648);
