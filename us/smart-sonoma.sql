8d3456a8a2e6a7e05300f5d9e87ec2cbffc53851
Peter Storch <peter.storch@tuta.io>
2022-08-02 13:32:23 +0200

    Add stationId and create insert statements

---
+++ us/smart-sonoma.sql
@@ -0,0 +1,12 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1577', 'Cotati', 38.33095968, -122.6910795);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1578', 'Larkspur', 37.94780852, -122.5124853);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1579', 'Marin Civic Center', 38.00124882, -122.5377642);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1580', 'Novato Downtown', 38.1059839, -122.5644475);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1581', 'Novato Hamilton', 38.0562882, -122.5238668);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1582', 'Novato San Marin', 38.12090369, -122.5660712);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1583', 'Petaluma Downtown', 38.23725105, -122.6348266);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1584', 'Rohnert Park', 38.34710199, -122.7010416);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1585', 'San Rafael', 37.97200217, -122.5226592);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1586', 'Santa Rosa Downtown', 38.43757456, -122.7217602);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1587', 'Santa Rosa North', 38.45522778, -122.736454);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1588', 'Sonoma County Airport', 38.50982041, -122.7840805);
