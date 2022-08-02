5ceed61f8bb2f81caf0168cf123c444d2eafc1fd
Peter Storch <peter.storch@tuta.io>
2022-08-02 20:19:43 +0200

    Add stationId and insert statements for sound-transit

---
+++ us/sound-transit.sql
@@ -0,0 +1,7 @@
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1719', 'Mukilteo', 47.94837, -122.301052, 'SN03');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1720', 'Kent', 47.384154, -122.233033, 'SS03');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1721', 'Auburn', 47.306411, -122.232257, 'SS04');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1722', 'Sumner', 47.201287, -122.244813, 'SS05');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1723', 'Puyallup', 47.192717, -122.295675, 'SS06');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1724', 'South Tacoma', 47.203491, -122.485552, 'SS08');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1725', 'Lakewood', 47.153189, -122.499103, 'SS09');
