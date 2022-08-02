d34c9574f31ec94b74dd71c3368f18d9a1c6b797
Peter Storch <peter.storch@tuta.io>
2022-08-02 18:31:36 +0200

    Add stationId and insert statements for northstar

---
+++ us/northstar.sql
@@ -0,0 +1,7 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1644', 'Target Field', 44.983385, -93.276823);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1645', 'Fridley', 45.078921, -93.270856);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1646', 'Coon Rapids Riverdale', 45.190833, -93.351232);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1647', 'Anoka', 45.207781, -93.384071);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1648', 'Elk River', 45.282621, -93.542206);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1649', 'Big Lake', 45.329832, -93.730028);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1650', 'Ramsey', 45.231795, -93.461286);
