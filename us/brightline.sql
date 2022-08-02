a032ee6ecf9f52a150eeb973a765d3b962d593bc
Peter Storch <peter.storch@tuta.io>
2022-08-02 18:19:46 +0200

    Add stationId and insert statements for brightline

---
+++ us/brightline.sql
@@ -0,0 +1,3 @@
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1615', 'Fort Lauderdale', 26.1236190796, -80.1455459595, 'FBT');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1616', 'MiamiCentral', 25.7799682617, -80.195640564, 'EKW');
+insert into stations (countryCode, id, title, lat, lon, DS100) values ('us', '1617', 'West Palm Beach', 26.712015152, -80.0553588867, 'WPT');
