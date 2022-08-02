2150b7f2eced764198888a27fbc81ae91f9145f9
Peter Storch <peter.storch@tuta.io>
2022-08-02 19:37:26 +0200

    Add stationId and insert statements for Texas systems

---
+++ us/dallas.sql
@@ -0,0 +1,7 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1686', 'Medical/Market Center', 32.80986, -96.84187);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1687', 'Downtown Irving/Heritage Crossing', 32.814935, -96.947688);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1688', 'West Irving', 32.816127, -97.00757);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1689', 'CentrePort/DFW Airport', 32.816982, -97.053509);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1690', 'Bell', 32.804583, -97.155753);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1691', 'Richland Hills', 32.799468, -97.223415);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1692', 'Victory', 32.789903, -96.812363);
---
+++ us/trinity-metro.sql
@@ -0,0 +1,8 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1699', 'Fort Worth T&P Station', 32.745327, -97.328451);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1700', 'North Side', 32.795827, -97.337619);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1701', 'Mercantile Center', 32.819463, -97.293636);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1702', 'North Richland Hills/Iron Horse', 32.844651, -97.248921);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1703', 'North Richland Hills/Smithfield', 32.865039, -97.211384);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1704', 'Grapevine-Main Street', 32.933202, -97.077264);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1705', 'DFW Airport North', 32.933399, -97.054133);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1706', 'DFW Airport Terminal B', 32.906253, -97.041287);
