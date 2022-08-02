0d5bdcdfa151daafaf4f240e51ca8a4a6bb6a7a5
Peter Storch <peter.storch@tuta.io>
2022-08-02 18:45:11 +0200

    Add stationId and insert statements for wes

---
+++ us/wes.sql
@@ -0,0 +1,5 @@
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1665', 'Beaverton Transit Center', 45.49064, -122.801157);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1666', 'Hall/Nimbus', 45.457587, -122.786997);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1667', 'Tigard Transit Center', 45.429936, -122.769527);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1668', 'Tualatin', 45.383621, -122.764504);
+insert into stations (countryCode, id, title, lat, lon) values ('us', '1669', 'Wilsonville Transit Center', 45.311358, -122.775303);
